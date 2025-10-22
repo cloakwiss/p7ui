b579fc3a5be25921ce850af616a3850212f52658
Cloakwiss <sucodetr@protonmail.com>
2025-10-22 10:36:10 +0530

    Completed the layout and will add new data storage on backend

--- main.go
+++ main.go
@@ -12,29 +12,34 @@ import (
 	"github.com/starfederation/datastar-go/datastar"
 )
 
-//go:embed ui/static/index.html
-var index []byte
+var (
+	//go:embed ui/index.html
+	index []byte
 
-//go:embed ui/static/style.css
-var style []byte
+	//go:embed ui/style.css
+	style []byte
 
-//go:embed ui/static/datastar.js
-var datastarScript []byte
+	//go:embed ui/datastar.js
+	datastarScript []byte
+
+	//go:embed ui/drag.js
+	drag []byte
+)
 
 const port = 1337
 
-type target struct {
-	Executable string `json:"target"`
-	Hookdll    string `json:"hook"`
-}
+type (
+	target struct {
+		Executable string `json:"target"`
+		Hookdll    string `json:"hook"`
+	}
+	data struct {
+		name, section string
+	}
+)
 
 func main() {
 	router := chi.NewRouter()
-
-	const message = "Hello, world!"
-
-	var dataChan = make(chan string)
-	var closing = make(chan bool, 1)
 
 	{ // assets
 		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -50,7 +55,15 @@ func main() {
 			w.Header().Add("Content-Type", "text/javascript; charset=utf-8")
 			w.Write(datastarScript)
 		})
+
+		router.Get("/drag.js", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Content-Type", "text/javascript; charset=utf-8")
+			w.Write(drag)
+		})
 	}
+
+	var dataChan = make(chan data)
+	var closing = make(chan bool, 1)
 
 	router.Post("/target_pick", func(w http.ResponseWriter, r *http.Request) {
 		var target = target{}
@@ -67,14 +80,7 @@ func main() {
 	})
 
 	router.Post("/spawnp7", func(w http.ResponseWriter, r *http.Request) {
-		sse := datastar.NewSSE(w, r)
-		modeOpt := datastar.WithModeAppend()
-		container := datastar.WithSelectorID("console-output")
-		for message := range dataChan {
-			if err := sse.PatchElements(`<div>`+message+`</div>`, modeOpt, container); err != nil {
-				return
-			}
-		}
+		mainLoop(w, r, dataChan)
 	})
 
 	router.Post("/stop", func(w http.ResponseWriter, r *http.Request) {
@@ -83,31 +89,45 @@ func main() {
 
 	log.Printf("Starting server on http://localhost:%d", port)
 
-	go func() {
-		i := 0
-	main:
-		for {
-			select {
-			case <-closing:
-				close(closing)
-				break main
-			default:
-				{
-					time.Sleep(1 * time.Second)
-					if i < len(message) {
-						dataChan <- message[:i]
-					} else {
-						dataChan <- message
-					}
-					i++
-				}
-			}
-		}
-		// dataChan <- "Done"
-		close(dataChan)
-	}()
+	go feedChannel(dataChan, closing)
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), router); err != nil {
 		panic(err)
 	}
+}
+
+func mainLoop(w http.ResponseWriter, r *http.Request, dataChan <-chan data) {
+	sse := datastar.NewSSE(w, r)
+	modeOpt := datastar.WithModeAppend()
+	container := datastar.WithSelectorID("table-body")
+	for message := range dataChan {
+		formatted := fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>\n", message.name, message.section)
+		if err := sse.PatchElements(formatted, modeOpt, container); err != nil {
+			return
+		}
+	}
+
+}
+
+func feedChannel(dataChan chan<- data, control chan bool) {
+	const message = "Hello, world!"
+	i := 0
+main:
+	for {
+		select {
+		case <-control:
+			break main
+		default:
+			{
+				time.Sleep(250 * time.Millisecond)
+				if i < len(message) {
+					dataChan <- data{message, message}
+				} else {
+					dataChan <- data{message, message}
+				}
+				i++
+			}
+		}
+	}
+	close(control)
 }
---
+++ ui/drag.js
@@ -0,0 +1,51 @@
+///////////////////////////// Layout ///////////////////////////////////////////
+// Drag Resizer
+class DragResizer {
+  constructor(dragElement, containerElement) {
+    this.dragElement = dragElement;
+    this.containerElement = containerElement;
+    this.startX = null;
+    this.initWidth = null;
+    this.init();
+  }
+
+  init() {
+    this.dragElement.addEventListener("mousedown", (e) => this.onMouseDown(e));
+  }
+
+  onMouseDown(e) {
+    e.preventDefault();
+    this.startX = e.clientX;
+
+    const style = window.getComputedStyle(this.containerElement);
+    const width = style.getPropertyValue("--console-width");
+    this.initWidth = parseFloat(width) || 480;
+
+    document.addEventListener("mousemove", this.onMouseMove);
+    document.addEventListener("mouseup", this.onMouseUp);
+  }
+
+  onMouseMove = (e) => {
+    const offset = e.clientX - this.startX;
+    const newWidth = this.initWidth + offset;
+    this.containerElement.style.setProperty("--console-width", `${newWidth}px`);
+  };
+
+  onMouseUp = () => {
+    document.removeEventListener("mousemove", this.onMouseMove);
+    document.removeEventListener("mouseup", this.onMouseUp);
+  };
+}
+
+// Initialize
+const contentArea = document.getElementById("content-area");
+const resizeHandle = document.getElementById("resize-handle");
+const toggleButton = document.getElementById("toggle-detail");
+
+new DragResizer(resizeHandle, contentArea);
+
+toggleButton.addEventListener("click", () => {
+  const isShown = contentArea.dataset.showConsole === "true";
+  contentArea.dataset.showConsole = isShown ? "false" : "true";
+  toggleButton.textContent = isShown ? "Show Details" : "Hide Details";
+});
